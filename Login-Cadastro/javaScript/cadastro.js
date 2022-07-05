4628b768564fa2cd75208a55acbb9d0232e0bde8
Barbara Mattioly Andrade <103378004+barbaraMattioly@users.noreply.github.com>
2022-07-05 10:14:10 -0300

    ajustada pag index.html

--- Login-Cadastro/javaScript/cadastro.js
+++ Login-Cadastro/javaScript/cadastro.js
@@ -133,7 +133,7 @@ function cadastrarUsuario(){
 
             localStorage.setItem('listaUsuarios', JSON.stringify(listaUsuarios));
 
-            window.location.href = '../../../codigo/Login-Cadastro/login.html';
+            window.location.href = '../../../codigo/Login-Cadastro/index.html';
 
     }else{
         alert("Todos os campos devem estar preenchidos para efetuar o cadastro");
--- Pagina-Inicial/javaScript/logOut.js
+++ Pagina-Inicial/javaScript/logOut.js
@@ -1,10 +1,10 @@
 console.log(localStorage.getItem('token'));
 if(localStorage.getItem('token') == null){
     alert('Você precisa estar logado para acessar ao sistema');
-    window.location.href = '../../../codigo/Login-Cadastro/login.html';
+    window.location.href = '../../../codigo/Login-Cadastro/index.html';
 }
 
 function efetuarLogOut(){
     localStorage.removeItem('token');
-    window.location.href = '../../../codigo/Login-Cadastro/login.html';
+    window.location.href = '../../../codigo/Login-Cadastro/index.html';
 }
